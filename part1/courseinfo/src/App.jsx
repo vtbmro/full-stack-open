ae64ebedcd45aabe883e092f3a29823ba85c044c
Vicente <vt.bmro@gmail.com>
2024-01-23 12:01:01 +0100

    course information, part 2, exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,11 +6,7 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
-  <div>
-    <p>{props.part1} {props.exercise1}</p>
-    <p>{props.part2} {props.exercise2}</p>
-    <p>{props.part3} {props.exercise3}</p>
-  </div>
+    <p>{props.part} {props.exercise}</p>
   )
 }
 
@@ -33,7 +29,9 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercise1 = {exercises1} part2={part2} exercise2 = {exercises2} part3={part3} exercise3 = {exercises3}/> 
+      <Content part = {part1} exercise = {exercises1}/>
+      <Content part = {part2} exercise = {exercises2}/>
+      <Content part = {part3} exercise = {exercises3}/>
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </div>
   )
